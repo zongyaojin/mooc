a1052be8f9848f7e1d00eb540cb696236f86b845
Jimbo <zongyaojin@outlook.com>
2020-07-13 13:20:17 -0500

    adds section 4

---
+++ section_4/challenges/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    cout << "Enter you favorite number between 1 and 100: ";
+    int temp;
+    cin >> temp;
+    cout << "Amazing! That's my favorite number too!" << endl;
+    cout << "No really! " << temp << " is my favorite number!" << endl;
+	return 0;
+}
