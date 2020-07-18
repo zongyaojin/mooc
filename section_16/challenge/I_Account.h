acd41d0e84148c51644fb14c87ebde977a189726
Jimbo <zongyaojin@outlook.com>
2020-07-18 14:43:37 -0500

    adds comments about virtual destructor for polymorphic base classes

--- section_16/challenge/I_Account.h
+++ section_16/challenge/I_Account.h
@@ -14,6 +14,10 @@ public:
     virtual bool withdraw(double amount) = 0;
     virtual void print(std::ostream &os) const = 0;
     virtual ~I_Account() = default;
+    // Always make the destructor virtual when a class is meant to be used polymorphically
+    // Otherwise there could be resource leaks when you delete a derived class through a base class pointer
+    // Coz the destructor could be statically-bound to the non-virtual base class destructor
+    // Which is gonna slice the derived the class and possibly leak stuff
 };
 
 I_Account::I_Account(std::string name, double balance) 
