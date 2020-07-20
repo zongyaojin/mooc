65c6a695b654107cfc42cced3cb6267cbe82a31f
Jimbo <zongyaojin@outlook.com>
2020-07-20 14:21:44 -0500

    adds section 19 challenge 2

---
+++ section_19/challenge2/main.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+int main() 
+{
+    ifstream in_file {"../responses.txt"};
+    
+    if (!in_file.is_open()) {
+        cout << "Cannot open file.\n" << endl;
+        return 1;
+    }
+    
+    cout << setw(10) << left << "Student" << setw(10) << right << "Score" << endl;
+    cout << setw(20) << setfill('-') << "" << endl;
+    cout << setfill(' ');
+    
+    string key, answer, student;
+    int score{0}, count{0};
+    double total{0};
+    
+    getline(in_file, key);
+    while (!in_file.eof()) {
+        getline(in_file, student);
+        getline(in_file, answer);
+        
+        score = 0;
+        for (size_t i=0; i<key.size(); ++i) {
+            if (key.at(i) == answer.at(i))
+                ++score;
+        }
+        
+        cout << setw(10) << left << student << setw(10) << right << score << endl;
+        ++count;
+        total += score;
+    }
+    
+    cout << setw(20) << setfill('-') << "" << endl;
+    cout << setfill(' ');
+    cout << setw(10) << left << "Average" << setw(10) << right << total/count << endl;
+    
+    
+    cout << endl;
+    
+	return 0;
+}
