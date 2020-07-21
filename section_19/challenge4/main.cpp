3abbfc574d070d1cb3a9b52774c1ba6c2a651c74
Jimbo <zongyaojin@outlook.com>
2020-07-21 10:24:56 -0500

    adds section 19 challenge 4

---
+++ section_19/challenge4/main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <fstream>
+#include <iomanip>
+
+using namespace std;
+
+int main() 
+{
+    ifstream in_file {"../romeoandjuliet.txt"};
+    ofstream out_file {"../romeoandjuliet_out.txt"};
+    
+    if (!in_file) {
+        cerr << "File open error.\n" << endl;
+        return 1;
+    }
+    
+    if (!out_file) {
+        cerr << "File create error.\n" << endl;
+        return 1;
+    }
+    
+    int line {1};
+    string buff;
+    
+    while (getline(in_file, buff))
+        out_file << setw(10) << left << line++ << buff << endl;            
+    
+    in_file.close();
+    out_file.close();
+    cout << "Task completed.\n" << endl;
+	return 0;
+}
