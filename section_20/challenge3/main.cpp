636a4fc093a0ca3089400e873c8e671b0b6d6022
Jimbo <zongyaojin@outlook.com>
2020-07-22 14:16:45 -0500

    adds one-liner solution and comments for section 20 challenge 4

--- section_20/challenge3/main.cpp
+++ section_20/challenge3/main.cpp
@@ -58,6 +58,12 @@ void part1() {
                     words.insert(std::make_pair(word, 1));
                 else
                     ++words[word];
+                
+                // Or one can simply use the following one-liner, since according to cplusplus.com:
+                // `` a call to the std::map::operator[] function is equivalent to: (*((this->insert(make_pair(k,mapped_type()))).first)).second ``
+                // Which means it does the check for you and decide whether to create an element based on default initial value or modify existing element.
+                
+                // ++words[words];
             }
         }
         
@@ -84,6 +90,12 @@ void part2() {
                     words.insert(std::make_pair(word, std::set<int>{line_num}));
                 else
                     words[word].insert(line_num);
+                
+                // Or you can simply use the following one-liner, since according to cplusplus.com:
+                // `` a call to the std::map::operator[] function is equivalent to: (*((this->insert(make_pair(k,mapped_type()))).first)).second ``
+                // Which means it does the check for you and decide whether to create an element based on default initial value or modify existing element.
+                 
+                // words[word].insert(line_num);
             }
             ++line_num;
         }
