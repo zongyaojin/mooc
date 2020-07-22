fc52b3b6eb63051e03d12c840a9449a5f58a58dc
Jimbo <zongyaojin@outlook.com>
2020-07-22 13:52:47 -0500

    adds section 20 challenge 3

---
+++ section_20/challenge3/main.cpp
@@ -0,0 +1,103 @@
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <map>
+#include <set>
+#include <string>
+#include <iomanip>
+
+// Used for Part1: display the word and count from the std::map<std::string, int>
+void display_words(const std::map<std::string, int> &words) {
+    std::cout << std::setw(12) << std::left << "\nWord"
+                << std::setw(7) << std::right << "Count"<< std::endl;
+    std::cout << "===================" << std::endl;
+    for (auto pair: words)
+        std::cout << std::setw(12) << std::left << pair.first 
+                       << std::setw(7) << std::right << pair.second << std::endl;
+}
+
+// Used for Part2: display the word and occurences from the std::map<std::string, std::set<int>>
+void display_words(const std::map<std::string, std::set<int>> &words)
+{
+     std::cout << std::setw(12) << std::left << "\nWord"
+                << "Occurrences"<< std::endl;
+    std::cout << "=====================================================================" << std::endl;
+    for (auto pair: words) {
+        std::cout << std::setw(12) << std::left << pair.first 
+                       << std::left << "[ ";
+        for (auto i: pair.second) 
+            std::cout << i << " ";
+        std::cout << "]" << std::endl;
+    }
+}
+
+// This function removes periods, commas, semicolons and colon in a string and returns the clean version
+std::string clean_string(const std::string &s) {
+    std::string result;
+    for (char c: s) {
+        if (c == '.' || c == ',' || c == ';' || c == ':')
+            continue;
+        else
+            result += c;
+    }
+    return result;
+}
+
+// Part1 process the file and builds a map of words and the number of times they occur in the file
+void part1() {
+    std::map<std::string, int> words;
+    std::string line;       
+    std::string word;   
+    std::ifstream in_file {"../words.txt"};
+    if (in_file) {
+        
+        while (std::getline(in_file, line)) {
+            std::stringstream ss {clean_string(line)};
+            while (ss >> word) {
+                if (words.find(word) == words.end())
+                    words.insert(std::make_pair(word, 1));
+                else
+                    ++words[word];
+            }
+        }
+        
+        in_file.close();
+        display_words(words);
+    } else {
+        std::cerr << "Error opening input file" << std::endl;
+    }
+}
+    
+// Part2 process the file and builds a map of words and a set of line numbers in which the word appears
+void part2() {
+    std::map<std::string, std::set<int>> words;
+    std::string line;
+    std::string word;
+    std::ifstream in_file {"../words.txt"};
+    if (in_file) {
+     
+        int line_num {1};
+        while (std::getline(in_file, line)) {
+            std::stringstream ss {clean_string(line)};
+            while (ss >> word) {
+                if (words.find(word) == words.end())
+                    words.insert(std::make_pair(word, std::set<int>{line_num}));
+                else
+                    words[word].insert(line_num);
+            }
+            ++line_num;
+        }
+        
+        in_file.close();
+        display_words(words);
+    } else {
+        std::cerr << "Error opening input file" << std::endl;
+    }
+}
+
+int main() {
+    part1();
+    part2();
+    return 0;
+}
+
